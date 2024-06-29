220fb37cbe0a2b84b2a0eef3d0c1d72c241e1a2e
Maftun-siam <maftunhasans@gmail.com>
2024-06-29 23:58:33 +0600

    added

---
+++ bistro boss client menu/laptop/laptop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const laptop = () => {
+    return (
+        <div>
+            {/* Code Written from my dekstop */}
+        </div>
+    );
+};
+
+export default laptop;
