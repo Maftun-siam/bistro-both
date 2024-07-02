a2199acf2596d1f1cd9d9e0448d39cbd5028dbd0
Maftun-siam <maftunhasans@gmail.com>
2024-07-02 13:19:41 +0600

    Added cart

--- bistro boss client menu/src/pages/Shared/NavBar/NavBar.jsx
+++ bistro boss client menu/src/pages/Shared/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 
+
 const NavBar = () => {
     const { user, logOut } = useContext(AuthContext);
 
@@ -18,6 +19,14 @@ const NavBar = () => {
         <li><Link to="/order/salad">Order Food</Link></li>
         <li><Link to="/secret">Secret</Link></li>
         <li><Link to="/signup">Sign Up</Link></li>
+        <li>
+            <Link to="/">
+                <button className="btn">
+                    Cart
+                    <div className="badge badge-secondary">+99</div>
+                </button>
+            </Link>
+        </li>
         {
             user ? <>
                 <button onClick={handleLogOut} className="btn btn-ghost">Logout</button>
